35340cad76816c83ded99960c395779c6425aab1
11cdh <104803789+11cdh@users.noreply.github.com>
2022-06-02 18:14:58 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -515,101 +515,262 @@ Make_Default
 
  case $location in
       1) if [[ "$a1" != "$blue" ]]; then
-			a1=$white 
-		 fi        ;;  
+      		a1=$white 
+	 fi	;; 
+	 
       2) if [[ "$a2" != "$blue" ]]; then
-			a2=$white 
-		 fi        ;;  
+		a2=$white 
+	 fi     ;;  
+	 
       3) if [[ "$a3" != "$blue" ]]; then
-			a3=$white 
-		 fi        ;;  
+		a3=$white 
+	 fi     ;;  
+	 
       4) if [[ "$a4" != "$blue" ]]; then
-			a4=$white 
-		 fi        ;;  
+		a4=$white 
+	 fi     ;;  
+		 
       5) if [[ "$a5" != "$blue" ]]; then
-			a5=$white 
-		 fi        ;;  
+		a5=$white 
+	 fi     ;;  
+	 
       6) if [[ "$a6" != "$blue" ]]; then
-			a6=$white 
-		 fi        ;;  
-      7) if [[ "$a6" != "$blue" ]]; then
-			a6=$white 
-		 fi        ;;  
-      1) if [[ "$a1" != "$blue" ]]; then
-			a1=$white 
-		 fi        ;;  
-      1) if [[ "$a1" != "$blue" ]]; then
-			a1=$white 
-		 fi        ;;  
-      1) if [[ "$a1" != "$blue" ]]; then
-			a1=$white 
-		 fi        ;;  
-      1) if [[ "$a1" != "$blue" ]]; then
-			a1=$white 
-		 fi        ;;  
-       2) a2=$white ;;             
-      3) a3=$white ;;        
-      4) a4=$white ;;   
-      5) a5=$white ;;   
-	  6) a6=$white ;;   
-      7) a7=$white ;;   
-      8) a8=$white ;;   
-      9) b1=$white ;;   
-      10) b2=$white ;;   
-      11) b3=$white ;;   
-      12) b4=$white ;;   
-      13) b5=$white ;;   
-      14) b6=$white ;;   
-      15) b7=$white ;;   
-      16) b8=$white ;;   
-      17) c1=$white ;;   
-      18) c2=$white ;;   
-      19) c3=$white ;;   
-      20) c4=$white ;;   
-      21) c5=$white ;;   
-      22) c6=$white ;;   
-      23) c7=$white ;;   
-      24) c8=$white ;;
-      25) d1=$white ;;
-	  26) d2=$white ;;
-      27) d3=$white ;;
-      28) d4=$white ;;
-      29) d5=$white ;;
-      30) d6=$white ;;
-      31) d7=$white ;;
-      32) d8=$white ;;
-      33) e1=$white ;;
-      34) e2=$white ;;
-      35) e3=$white ;;
-      36) e4=$white ;;
-      37) e5=$white ;;
-      38) e6=$white ;;
-      39) e7=$white ;;
-      40) e8=$white ;;
-      41)   f1=$white ;;
-      42)   f2=$white ;;
-      43)	f3=$white ;;
-      44) 	f4=$white ;;
-      45) 	f5=$white ;;
-      46) 	f6=$white ;;
-      47) 	f7=$white ;;
-      48) 	f8=$white ;;
-      49) 	g1=$white ;;
-      50) 	g2=$white ;;
-      51)   g3=$white ;;
-      52)	g4=$white ;;
-      53) 	g5=$white ;;
-      54) 	g6=$white ;;
-      55) 	g7=$white ;;
-      56) 	g8=$white ;;
-      57) 	h1=$white ;;
-      58) 	h2=$white ;;
-      59)	h3=$white ;;
-      60) 	h4=$white ;;
-      61) 	h5=$white ;;
-      62) 	h6=$white ;;
-      63) 	h7=$white ;;
-	  64)   h8=$white ;;   
+		a6=$white 
+	 fi     ;;  
+	 
+      7) if [[ "$a7" != "$blue" ]]; then
+		a7=$white 
+ 	 fi     ;;  
+	 
+      8) if [[ "$a8" != "$blue" ]]; then
+		a8=$white 
+	 fi     ;;  
+	 
+      9) if [[ "$b1" != "$blue" ]]; then
+	        b1=$white 
+	 fi     ;;  
+	 
+      10) if [[ "$b2" != "$blue" ]]; then
+		b2=$white 
+	 fi     ;;  
+	 
+      11) if [[ "$b3" != "$blue" ]]; then
+		b3=$white 
+	 fi     ;;  
+	 
+      12) if [[ "$b4" != "$blue" ]]; then
+		b4=$white 
+	 fi     ;;           
+	 
+      13) if [[ "$b5" != "$blue" ]]; then
+		b5=$white 
+	 fi     ;;    
+		 
+      14) if [[ "$b6" != "$blue" ]]; then
+		b6=$white 
+	 fi     ;;   
+		 
+      15) if [[ "$b7" != "$blue" ]]; then
+		b7=$white 
+	 fi     ;;   
+		 
+      16) if [[ "$b8" != "$blue" ]]; then
+		b8=$white 
+	 fi     ;;   
+		 
+      17) if [[ "$c1" != "$blue" ]]; then
+		c1=$white 
+	 fi     ;;   
+		 
+      18) if [[ "$c2" != "$blue" ]]; then
+		c2=$white 
+	 fi     ;;   
+		 
+      19) if [[ "$c3" != "$blue" ]]; then
+		c3=$white 
+	 fi     ;; 
+	 
+      20) if [[ "$c4" != "$blue" ]]; then
+      		c4=$white 
+	 fi	;; 
+	 
+      21) if [[ "$c5" != "$blue" ]]; then
+		c5=$white 
+	 fi     ;;  
+	 
+      22) if [[ "$c6" != "$blue" ]]; then
+		c6=$white 
+	 fi     ;;  
+	 
+      23) if [[ "$c7" != "$blue" ]]; then
+		c7=$white 
+	 fi     ;;  
+		 
+      24) if [[ "$c8" != "$blue" ]]; then
+		c8=$white 
+	 fi     ;;  
+	 
+      25) if [[ "$d1" != "$blue" ]]; then
+		d1=$white 
+	 fi     ;;  
+	 
+      26) if [[ "$d2" != "$blue" ]]; then
+		d2=$white 
+ 	 fi     ;;  
+	 
+      27) if [[ "$d3" != "$blue" ]]; then
+		d3=$white 
+	 fi     ;;  
+	 
+      28) if [[ "$d4" != "$blue" ]]; then
+	        d4=$white 
+	 fi     ;;  
+	 
+      29) if [[ "$d5" != "$blue" ]]; then
+		d6=$white 
+	 fi     ;;  
+	 
+      30) if [[ "$d6" != "$blue" ]]; then
+		d6=$white 
+	 fi     ;;  
+	 
+      31) if [[ "$d7" != "$blue" ]]; then
+		d7=$white 
+	 fi     ;;           
+	 
+      32) if [[ "$d8" != "$blue" ]]; then
+		d8=$white 
+	 fi     ;;    
+		 
+      33) if [[ "$e1" != "$blue" ]]; then
+		e1=$white 
+	 fi     ;;   
+		 
+      34) if [[ "$e2" != "$blue" ]]; then
+		e2=$white 
+	 fi     ;;   
+		 
+      35) if [[ "$e3" != "$blue" ]]; then
+		e3=$white 
+	 fi     ;;   
+		 
+      36) if [[ "$e4" != "$blue" ]]; then
+		e4=$white 
+	 fi     ;;   
+		 
+      37) if [[ "$e5" != "$blue" ]]; then
+		e5=$white 
+	 fi     ;; 
+	 
+      38) if [[ "$e6" != "$blue" ]]; then
+		e6=$white 
+	 fi     ;;  
+	 
+      39) if [[ "$e7" != "$blue" ]]; then
+		e7=$white 
+	 fi     ;;  
+	 
+      40) if [[ "$e8" != "$blue" ]]; then
+		e8=$white 
+	 fi     ;;  
+		 
+      41) if [[ "$f1" != "$blue" ]]; then
+		f1=$white 
+	 fi     ;;  
+	 
+      42) if [[ "$f2" != "$blue" ]]; then
+		f2=$white 
+	 fi     ;;  
+	 
+      43) if [[ "$f3" != "$blue" ]]; then
+		f3=$white 
+ 	 fi     ;;  
+	 
+      44) if [[ "$f4" != "$blue" ]]; then
+		f4=$white 
+	 fi     ;;  
+	 
+      45) if [[ "$f5" != "$blue" ]]; then
+	        f5=$white 
+	 fi     ;;  
+	 
+      46) if [[ "$f6" != "$blue" ]]; then
+		f6=$white 
+	 fi     ;;  
+	 
+      47) if [[ "$f7" != "$blue" ]]; then
+		f7=$white 
+	 fi     ;;  
+	 
+      48) if [[ "$f8" != "$blue" ]]; then
+		f8=$white 
+	 fi     ;;           
+	 
+      49) if [[ "$g1" != "$blue" ]]; then
+		g1=$white 
+	 fi     ;;    
+		 
+      50) if [[ "$g2" != "$blue" ]]; then
+		g2=$white 
+	 fi     ;;   
+		 
+      51) if [[ "$g3" != "$blue" ]]; then
+		g3=$white 
+	 fi     ;;   
+		 
+      52) if [[ "$g4" != "$blue" ]]; then
+		g4=$white 
+	 fi     ;;   
+		 
+      53) if [[ "$g5" != "$blue" ]]; then
+		g5=$white 
+	 fi     ;;   
+		 
+      54) if [[ "$g6" != "$blue" ]]; then
+		g6=$white 
+	 fi     ;;
+	 
+      55) if [[ "$g7" != "$blue" ]]; then
+		g7=$white 
+	 fi     ;;  
+	 
+      56) if [[ "$g8" != "$blue" ]]; then
+		g8=$white 
+	 fi     ;;  
+		 
+      57) if [[ "$h1" != "$blue" ]]; then
+		h1=$white 
+	 fi     ;;  
+	 
+      58) if [[ "$h2" != "$blue" ]]; then
+		h2=$white 
+	 fi     ;;  
+	 
+      59) if [[ "$h3" != "$blue" ]]; then
+		h3=$white 
+ 	 fi     ;;  
+	 
+      60) if [[ "$h4" != "$blue" ]]; then
+		h4=$white 
+	 fi     ;;  
+	 
+      61) if [[ "$h5" != "$blue" ]]; then
+	        h5=$white 
+	 fi     ;;  
+	 
+      62) if [[ "$h6" != "$blue" ]]; then
+		h6=$white 
+	 fi     ;;  
+	 
+      63) if [[ "$h7" != "$blue" ]]; then
+		h7=$white 
+	 fi     ;;  
+	 
+      64) if [[ "$h8" != "$blue" ]]; then
+		h8=$white 
+	 fi     ;;           
+	      
+	         
 esac
 }
 
@@ -627,246 +788,251 @@ if [[ "$a3" != "$blue" ]]; then
 	a3="   "
 fi
 
-if [[ $a4 != $blue ]]; then
+if [[ "$a4" != "$blue" ]]; then
 	a4="   "
 fi
-if [[ $a5 != $blue ]]; then
+
+if [[ "$a5" != "$blue" ]]; then
 	a5="   "
 fi
-if [[ $a6 != $blue ]]; then
+
+if [[ "$a6" != "$blue" ]]; then
 	a6="   "
 fi
-if [[ $a7 != $blue ]]; then
+
+if [[ "$a7" != "$blue" ]]; then
 	a7="   "
 fi
 
-if [[ $a8 != $blue ]]; then
+if [[ "$a8" != "$blue" ]]; then
 	a8="   "
 fi
 
-if [[ $b1 != $blue ]]; then
+if [[ "$b1" != "$blue" ]]; then
 	b1="   "
 fi
 
-if [[ $b2 != $blue ]]; then
+if [[ "$b2" != "$blue" ]]; then
 	b2="   "
 fi
 
-if [[ $b3 != $blue ]]; then
+if [[ "$b3" != "$blue" ]]; then
 	b3="   "
 fi
 
-if [[ $b4 != $blue ]]; then
+if [[ "$b4" != "$blue" ]]; then
 	b4="   "
 fi
 
-if [[ $b5 != $blue ]]; then
+if [[ "$b5" != "$blue" ]]; then
 	b5="   "
 fi
 
-if [[ $b6 != $blue ]]; then
+if [[ "$b6" != "$blue" ]]; then
 	b6="   "
 fi
 
-if [[ $b7 != $blue ]]; then
+if [[ "$b7" != "$blue" ]]; then
 	b7="   "
 fi
 
-if [[ $b8 != $blue ]]; then
+if [[ "$b8" != "$blue" ]]; then
 	b8="   "
 fi
 
-if [[ $c1 != $blue ]]; then
+if [[ "$c1" != "$blue" ]]; then
 	c1="   "
 fi
 
-if [[ $c2 != $blue ]]; then
+if [[ "$c2" != "$blue" ]]; then
 	c2="   "
 fi
 
-if [[ $c3 != $blue ]]; then
+if [[ "$c3" != "$blue" ]]; then
 	c3="   "
 fi
 
-if [[ $c4 != $blue ]]; then
+if [[ "$c4" != "$blue" ]]; then
 	c4="   "
 fi
 
-if [[ $c5 != $blue ]]; then
+if [[ "$c5" != "$blue" ]]; then
 	c5="   "
 fi
 
-if [[ $c6 != $blue ]]; then
+if [[ "$c6" != "$blue" ]]; then
 	c6="   "
 fi
 
-if [[ $c7 != $blue ]]; then
+if [[ "$c7" != "$blue" ]]; then
 	c7="   "
 fi
 
-if [[ $c8 != $blue ]]; then
+if [[ "$c8" != "$blue" ]]; then
 	c8="   "
 fi
 
-if [[ $d1 != $blue ]]; then
+if [[ "$d1" != "$blue" ]]; then
 	d1="   "
 fi
 
-if [[ $d2 != $blue ]]; then
+if [[ "$d2" != "$blue" ]]; then
 	d2="   "
 fi
 
-if [[ $d3 != $blue ]]; then
+if [[ "$d3" != "$blue" ]]; then
 	d3="   "
 fi
 
-if [[ $d4 != $blue ]]; then
+if [[ "$d4" != "$blue" ]]; then
 	d4="   "
 fi
 
-if [[ $d5 != $blue ]]; then
+if [[ "$d5" != "$blue" ]]; then
 	d5="   "
 fi
 
-if [[ $d6 != $blue ]]; then
+if [[ "$d6" != "$blue" ]]; then
 	d6="   "
 fi
 
-if [[ $d7 != $blue ]]; then
+if [[ "$d7" != "$blue" ]]; then
 	d7="   "
 fi
 
-if [[ $d8 != $blue ]]; then
+if [[ "$d8" != "$blue" ]]; then
 	d8="   "
 fi
 
-if [[ $e1 != $blue ]]; then
+if [[ "$e1" != "$blue" ]]; then
 	e1="   "
 fi
 
-if [[ $e2 != $blue ]]; then
+if [[ "$e2" != "$blue" ]]; then
 	e2="   "
 fi
 
-if [[ $e3 != $blue ]]; then
+if [[ "$e3" != "$blue" ]]; then
 	e3="   "
 fi
 
-if [[ $e4 != $blue ]]; then
+if [[ "$e4" != "$blue" ]]; then
 	e4="   "
 fi
 
-if [[ $e5 != $blue ]]; then
+if [[ "$e5" != "$blue" ]]; then
 	e5="   "
 fi
 
-if [[ $e6 != $blue ]]; then
+if [[ "$e6" != "$blue" ]]; then
 	e6="   "
 fi
 
-if [[ $e7 != $blue ]]; then
+if [[ "$e7" != "$blue" ]]; then
 	e7="   "
 fi
 
-if [[ $e8 != $blue ]]; then
+if [[ "$e8" != "$blue" ]]; then
 	e8="   "
 fi
 
-if [[ $f1 != $blue ]]; then
+if [[ "$f1" != "$blue" ]]; then
 	f1="   "
 fi
 
-if [[ $f2 != $blue ]]; then
+if [[ "$f2" != "$blue" ]]; then
 	f2="   "
 fi
 
-if [[ $f3 != $blue ]]; then
+if [[ "$f3" != "$blue" ]]; then
 	f3="   "
 fi
 
-if [[ $f4 != $blue ]]; then
+if [[ "$f4" != "$blue" ]]; then
 	f4="   "
 fi
 
-if [[ $f5 != $blue ]]; then
+if [[ "$f5" != "$blue" ]]; then
 	f5="   "
 fi
 
-if [[ $f6 != $blue ]]; then
+if [[ "$f6" != "$blue" ]]; then
 	f6="   "
 fi
 
-if [[ $f7 != $blue ]]; then
+if [[ "$f7" != "$blue" ]]; then
 	f7="   "
 fi
 
-if [[ $f8 != $blue ]]; then
+if [[ "$f8" != "$blue" ]]; then
 	f8="   "
 fi
 
-if [[ $g1 != $blue ]]; then
+if [[ "$g1" != "$blue" ]]; then
 	g1="   "
 fi
 
-if [[ $g2 != $blue ]]; then
+if [[ "$g2" != "$blue" ]]; then
 	g2="   "
 fi
 
-if [[ $g3 != $blue ]]; then
+if [[ "$g3" != "$blue" ]]; then
 	g3="   "
 fi
 
-if [[ $g4 != $blue ]]; then
+if [[ "$g4" != "$blue" ]]; then
 	g4="   "
 fi
 
-if [[ $g5 != $blue ]]; then
+if [[ "$g5" != "$blue" ]]; then
 	g5="   "
 fi
 
-if [[ $g6 != $blue ]]; then
+if [[ "$g6" != "$blue" ]]; then
 	g6="   "
 fi
 
-if [[ $g7 != $blue ]]; then
+if [[ "$g7" != "$blue" ]]; then
 	g7="   "
 fi
 
-if [[ $g8 != $blue ]]; then
+if [[ "$g8" != "$blue" ]]; then
 	g8="   "
 fi
 
-if [[ $h1 != $blue ]]; then
+if [[ "$h1" != "$blue" ]]; then
 	h1="   "
 fi
 
-if [[ $h2 != $blue ]]; then
+if [[ "$h2" != "$blue" ]]; then
 	h2="   "
 fi
 
-if [[ $h3 != $blue ]]; then
+if [[ "$h3" != "$blue" ]]; then
 	h3="   "
 fi
 
-if [[ $h4 != $blue ]]; then
+if [[ "$h4" != "$blue" ]]; then
 	h4="   "
 fi
 
-if [[ $h5 != $blue ]]; then
+if [[ "$h5" != "$blue" ]]; then
 	h5="   "
 fi
 
-if [[ $h6 != $blue ]]; then
+if [[ "$h6" != "$blue" ]]; then
 	h6="   "
 fi
 
-if [[ $h7 != $blue ]]; then
+if [[ "$h7" != "$blue" ]]; then
 	h7="   "
 fi
 
-if [[ $h8 != $blue ]]; then
+if [[ "$h8" != "$blue" ]]; then
 	h8="   "
 fi
 
 }
+
+
 Map1_Prepare
